a6bc15e2f1f85c765d4ed3ec20a5b40bed03fe5c
Hacker_big_brown <147969984+yangtommmmmmmm@users.noreply.github.com>
2025-12-24 10:49:31 +0800

    Update udp_send.py

--- OWL/UDP Test/udp_send.py
+++ OWL/UDP Test/udp_send.py
@@ -1,1 +1,16 @@
+#This python script is for sending out udp packet on OPDS-1000.
+#!/usr/bin python3
+import socket
 
+#Define the received IP,PORT of host
+UDP_IP = "10.10.X.93"
+UDP_PORT = 12345
+
+MESSAGE = "Test UDP"
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+#Show the successful sending
+for i in range(1000):
+    sock.sendto(MESSAGE.encode(), (UDP_IP,UDP_PORT))
+    print("Sent message to", UDP_IP, UDP_PORT)
